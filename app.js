7d16ba4cb434b8c65412fc2f08da52169decb003
McDowra <rmcdowra@una.edu>
2024-08-29 13:12:27 -0500

    added my docs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ console.log('im on a node server, yo');
 const express = require('express')
 const app = express()
 
+app.use(express.static('./public/'))
+
 app.get('/', function (req, res) {
   // res.send('Hello World')
   res.sendFile('index.html')
